08ffa7b551e60c4b15807e2e3f6a57253ee1a391
Leon <leon.hebeisen08@gmail.com>
2025-12-05 22:41:53 +0100

    [Update] styling and grid

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function darkmode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 } 
+function submitMail() {
+  alert("Thank you for your message. I will get back to you shortly!");
+}
